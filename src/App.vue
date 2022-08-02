e85c2f613f74d2ec1d7b1ebd831df4e48d810591
Kimio Nishino <kimionishino1@gmail.com>
2022-08-02 00:38:43 -0400

    Starting from scratch
    Decided to take a new route in this small project

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,30 @@
 <template>
-    <div class="app">
-        <NavbarComponent />
-        <p>Hello</p>
-    </div>
-    <router-view />
+  <nav>
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+  </nav>
+  <router-view />
 </template>
 
-<script>
-import NavbarComponent from './components/NavbarComponent.vue'
-export default {
-    name: 'App',
-    components: {
-        NavbarComponent
-    }
-}
-</script>
 <style>
-* {
-    padding: 0;
-    margin: 0;
-    box-sizing: border-box;
-    font-family: "Raleway", sans-serif;
-    font-weight: 400;
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
 }
 
-.app {
-    min-height: 100vh;
-    position: relative;
-    background-color: #f1f1f1;
-    border: 1px solid red;
+nav {
+  padding: 30px;
+}
+
+nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+nav a.router-link-exact-active {
+  color: #42b983;
 }
 </style>
